55bd7742351f61e1d3888253b5204cc4856ad98d
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2023-02-10 23:41:08 -0800

    i

---
+++ app/src/main/java/com/example/budgetbuddy/SigninActivity.java
@@ -0,0 +1,14 @@
+package com.example.budgetbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+    }
+}
