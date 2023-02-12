66b76e38d3c684333f93274bbe606ec973423a85
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2023-02-11 20:30:10 -0800

    Use text layout for input

--- app/src/main/java/com/example/budgetbuddy/LoginActivity.java
+++ app/src/main/java/com/example/budgetbuddy/LoginActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SigninActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signin);
+        setContentView(R.layout.activity_login);
         ActionBar actionBar = getSupportActionBar();
 
         assert actionBar != null;
--- app/src/main/java/com/example/budgetbuddy/MainActivity.java
+++ app/src/main/java/com/example/budgetbuddy/MainActivity.java
@@ -5,13 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSignUp;
-    Button btnLogin;
+    private Button btnSignUp;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp = findViewById(R.id.sign_up_btn);
 
         btnLogin.setOnClickListener(view -> {
-            Intent login = new Intent(MainActivity.this, SigninActivity.class);
+            Intent login = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(login);
         });
 
