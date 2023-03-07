facae3674de7934f931505f5694e3ef77a5a097e
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2023-03-07 15:02:33 -0800

    check that a user is logged in when activity starts or on resume

--- app/src/main/java/com/example/budgetbuddy/ExpenseActivity.java
+++ app/src/main/java/com/example/budgetbuddy/ExpenseActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ExpenseActivity extends AppCompatActivity {
+public class ExpenseActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/budgetbuddy/SignupActivity.java
+++ app/src/main/java/com/example/budgetbuddy/SignupActivity.java
@@ -20,7 +20,7 @@ import com.example.budgetbuddy.utils.PasswordHelper;
 
 import static android.content.ContentValues.TAG;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends BaseActivity {
    private EditText email;
     private EditText userName;
     private EditText password;
