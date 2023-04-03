4610c15e012d49dc2e81232f90708e42c19197b4
Akinpelu7 <Omotosoe@student.douglascollege.ca>
2023-04-03 04:57:36 -0700

    added the budget class , budget recycler view to display the budget and the UI

---
+++ app/src/main/java/com/example/budgetbuddy/ui/adapters/BudgetRecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package com.example.budgetbuddy.ui.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.budgetbuddy.R;
+import com.example.budgetbuddy.ui.budget.Budget;
+
+import java.util.List;
+
+public class BudgetRecyclerViewAdapter extends RecyclerView.Adapter<BudgetRecyclerViewAdapter.ImageViewHolder> {
+
+    List<Budget> BudgetList;
+
+    public BudgetRecyclerViewAdapter(List<Budget> budgetList) {
+        BudgetList = budgetList;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_budget_item,parent,false);
+
+       ImageViewHolder holder = new ImageViewHolder(itemView);
+
+       return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+
+        holder.imageViewItem.setImageResource(BudgetList.get(position).getBudgetPic());
+        holder.textViewItem.setText(BudgetList.get(position).getBudgetName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return BudgetList.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageViewItem;
+        TextView textViewItem;
+        EditText editTextItem;
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+           imageViewItem= itemView.findViewById(R.id.imgBudgetItem);
+           textViewItem = itemView.findViewById(R.id.txtViewBudgetItem);
+           editTextItem = itemView.findViewById(R.id.editTextBudgetItem);
+
+        }
+    }
+}
--- app/src/main/java/com/example/budgetbuddy/ui/budget/AddBudgetFragment.java
+++ app/src/main/java/com/example/budgetbuddy/ui/budget/AddBudgetFragment.java
@@ -4,17 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.budgetbuddy.R;
 import com.example.budgetbuddy.databinding.FragmentAddBudgetBinding;
 import com.example.budgetbuddy.databinding.FragmentHomeBinding;
+import com.example.budgetbuddy.ui.adapters.BudgetRecyclerViewAdapter;
+import com.example.budgetbuddy.ui.add_expense.Category;
 import com.example.budgetbuddy.ui.home.HomeViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddBudgetFragment extends Fragment {
+    List<Budget> budgets = new ArrayList<>();
+    EditText editTextBudgetAmount;
+    RecyclerView recyclerViewBudget;
+    Button btnAddBudget;
     private FragmentAddBudgetBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,9 +38,16 @@ public class AddBudgetFragment extends Fragment {
 
         binding = FragmentAddBudgetBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        recyclerViewBudget =binding.recyclerViewBudget;
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerViewBudget.setLayoutManager(layoutManager);
+        BudgetRecyclerViewAdapter adapter = new BudgetRecyclerViewAdapter(budgets);
+        recyclerViewBudget.setAdapter(adapter);
 
-        final TextView textView = binding.textAddBudget;
-        ViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        AddData();
+
+        //final TextView textView = binding.textAddBudget;
+      //  ViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -34,5 +55,17 @@ public class AddBudgetFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void AddData() {
+        budgets.add(new Budget(101, "Housing", R.drawable.housing,0));
+        budgets.add(new Budget(102, "Transportation", R.drawable.transportation,0));
+        budgets.add(new Budget(103, "Food and Dining", R.drawable.feeding,0));
+        budgets.add(new Budget(104, "Utilities", R.drawable.utilities,0));
+        budgets.add(new Budget(105, "Insurance", R.drawable.insurance,0));
+        budgets.add(new Budget(106, "Medical and Healthcare", R.drawable.medical,0));
+        budgets.add(new Budget(104, "Personal Spending", R.drawable.personal_spending,0));
+        budgets.add(new Budget(105, "Recreation and Entertainment", R.drawable.entertainment,0));
+        budgets.add(new Budget(106, "Miscellaneous", R.drawable.miscellaneous,0));
     }
 }
